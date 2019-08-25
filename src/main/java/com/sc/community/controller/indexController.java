aaf63feeb9e3a90f5bc8ecc55257fb06c8824aec
pigdou <33105759@qq.com>
2019-08-25 10:53:15 +0800

    add index and bootstrap

--- src/main/java/com/sc/community/controller/indexController.java
+++ src/main/java/com/sc/community/controller/indexController.java
@@ -1,19 +1,19 @@
 package com.sc.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Auther: An
  * @Date: Created in 11:012019/8/24
  * @Description:
  */
-@RestController
-public class hello {
+@Controller
+public class indexController{
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
